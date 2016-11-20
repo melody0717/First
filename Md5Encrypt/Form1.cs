de1a6be7af17c4c982e53d89016533b299d5a3a7
Melody <hzhhhbb@outlook.com>
2016-11-20 22:46:13 +0800

    测试git

--- Md5Encrypt/Form1.cs
+++ Md5Encrypt/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Md5Encrypt
 {
@@ -15,6 +16,29 @@ namespace Md5Encrypt
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text.Trim()))
+            {
+                textBox1.Focus();
+            }
+            else
+            {
+                byte[] b = Encoding.Default.GetBytes(textBox1.Text.Trim());
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] output = md5.ComputeHash(b);
+                this.textBox2.Text = BitConverter.ToString(output).Replace("-", "");
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+
         }
     }
 }
